bffc1204cb00a3da593f6b1047b6444ab174890f
Isonzo <renato.soverina@gmail.com>
2024-03-18 02:13:58 -0500

    No worries, be happy

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project-id.firebaseapp.com",
-  databaseURL: "https://your-project-id.firebaseio.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id",
-  measurementId: "your-measurement-id"
+const firebaseConfig = {
+  apiKey: "AIzaSyBXxxDq2qqtoO9K_VWOfQTKogm1k6-wxTg",
+  authDomain: "jelly-blaster.firebaseapp.com",
+  projectId: "jelly-blaster",
+  storageBucket: "jelly-blaster.appspot.com",
+  messagingSenderId: "339213151254",
+  appId: "1:339213151254:web:dae8a1c9d52a4bc3659937"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
+const firestore = getFirestore(app);
+
+export default firestore
